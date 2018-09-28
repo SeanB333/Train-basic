2ea39b3f27aa0d06c19deeca646297cca23c8893
SeanB <Spb4578@gmail.com>
2018-09-28 01:07:30 -0500

    adding the firebase inputs

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,18 +1,44 @@
-$(document).ready(function() {
-    $("#train-name").on("click", function(event){
+$(document).ready(function () {
+
+    var config = {
+        apiKey: "AIzaSyBc_admZir4gKCOodym4IudAJ3vkh5lxjY",
+        authDomain: "train-basic.firebaseapp.com",
+        databaseURL: "https://train-basic.firebaseio.com",
+        projectId: "train-basic",
+        storageBucket: "train-basic.appspot.com",
+        messagingSenderId: "91262479364"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+    var trainName = "";
+    var dest = "";
+    var trainTime = "";
+    var frequency = "";
+
+    $("#add-train").on("click", function (event) {
         event.preventDefault();
-        var trainName = $("#train-input").val().trim();
-        var dest = $("#destination-input").val().trim();
-        var trainTime = $("#time-input").val().trim();
-        var frequency = $("#freq-input").val().trim();
+
+        trainName = $("#train-input").val().trim();
+        dest = $("#destination-input").val().trim();
+        trainTime = $("#time-input").val().trim();
+        frequency = $("#freq-input").val().trim();
 
         console.log(trainName);
         console.log(dest);
         console.log(trainTime);
         console.log(frequency);
 
-        
-        
+        database.ref().set({
+            name: trainName,
+            destination: dest,
+            time: trainTime,
+            frequency: frequency
+        })
+    })
 
+    database.ref().on("value", function(snapshot){
+        snap = snapshot.val();
+        
     })
 })
