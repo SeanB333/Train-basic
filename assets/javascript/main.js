a85c2321147bf73e71c00d88b41c4b0d589823a0
SeanB333 <spb4578@gmail.com>
2018-09-27 23:31:12 -0500

    ghanging file directories

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-    
+    $("#train-name").on("click", function(event){
+        event.preventDefault();
+        var trainName = $("#train-input").val().trim();
+        var dest = $("#destination-input").val().trim();
+        var trainTime = $("#time-input").val().trim();
+        var frequency = $("#freq-input").val().trim();
+
+        console.log(trainName);
+        console.log(dest);
+        console.log(trainTime);
+        console.log(frequency);
+
+        
+        
+
+    })
 })
