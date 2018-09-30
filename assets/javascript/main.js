6fa989153a57075461712106b6529e239c1f1172
SeanB <Spb4578@gmail.com>
2018-09-30 02:22:39 -0500

    added form validation and button animation

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -28,24 +28,16 @@ $(document).ready(function () {
     var time = "Loading";
     $("#display-time").html(time);
 
-    
+
 
     // set functions
 
-    function myTimer() {
-        var date = new Date();
-        time = date.toLocaleTimeString();
-        $("#display-time").html(time);
-    }
-
-    // set values onlick to submit
-
-    $("#add-train").on("click", function (event) {
-        event.preventDefault();
+    function setData() {
         trainName = $("#train-input").val().trim();
         destination = $("#destination-input").val().trim();
         trainTime = $("#time-input").val().trim();
         frequency = $("#freq-input").val().trim();
+
 
         // difine firebase object
 
@@ -56,13 +48,59 @@ $(document).ready(function () {
             frequency: frequency
         });
 
-        
+        // clear values
+
+        $("#train-input").val("");
+        $("#destination-input").val("");
+        $("#time-input").val("");
+        $("#freq-input").val("");
+
+    }
+
+    function myTimer() {
+        var date = new Date();
+        time = date.toLocaleTimeString();
+        $("#display-time").html(time);
+    }
+
+    function formFail() {
+        $("#add-train").addClass("red");
+        $("#btnTrain").html("<strong>X</strong> - Error: please enter valid info");
+        setTimeout(function () {
+            $("#add-train").removeClass("red");
+            $("#btnTrain").html("Submit");
+        }, 3000);
+    }
+
+    function formPass() {
+        event.preventDefault();
+        $("#add-train").addClass("green");
+        $("#btnTrain").html("√ Train added");
+        setTimeout(function () {
+            $("#add-train").removeClass("green");
+            $("#btnTrain").html("Submit");
+        }, 3000);
+    }
+
+    // set values onlick to submit
+
+    $("#add-train").on("click", function (event) {
+        event.preventDefault();
+        if ($("#train-input").val() === "" || $("#destination-input").val() === "" || $("#time-input").val() === 0 || $("#freq-input").val() === 0) {
+            formFail();
+        } else {
+            formPass();
+            setData();
+            
+        }
+
+
     });
 
     // call database object to display
 
     database.ref().on("child_added", function (snapshot) {
-        
+
         // define firebase variables
 
         var snap = snapshot.val();
@@ -70,7 +108,7 @@ $(document).ready(function () {
         var timeRemain = moment().diff(convert, "minutes") % snap.frequency;
         var minAway = frequency - timeRemain;
         var arrival = moment().add(minAway, "m").format("h:mm:ss a");
-        
+
         // append the table values
 
         $(".input").append(
